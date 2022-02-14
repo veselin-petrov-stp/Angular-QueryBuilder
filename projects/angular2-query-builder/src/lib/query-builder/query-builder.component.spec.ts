3560e2ae6acd19d01ed71085b94a2a28fb9d2e60
Veselin.Petrov <veselin.petrov@STPAG2014.onmicrosoft.com>
2022-02-14 16:06:21 +0200

    angular updated to version 11

--- projects/angular2-query-builder/src/lib/query-builder/query-builder.component.spec.ts
+++ projects/angular2-query-builder/src/lib/query-builder/query-builder.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { CommonModule } from '@angular/common';
 import { FormsModule, } from '@angular/forms';
 import { QueryBuilderComponent } from './query-builder.component';
@@ -7,7 +7,7 @@ describe('QueryBuilderComponent', () => {
   let component: QueryBuilderComponent;
   let fixture: ComponentFixture<QueryBuilderComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [ CommonModule, FormsModule ],
       declarations: [ QueryBuilderComponent ]
